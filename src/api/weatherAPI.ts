6f1f6218fe1d98299ab5d263c2abe477aa53510b
GunSlinger <you@example.com>
2021-08-15 23:48:47 +0300

    add env file, change API keys

--- src/api/weatherAPI.ts
+++ src/api/weatherAPI.ts
@@ -1,32 +1,31 @@
-import axios, { AxiosInstance } from "axios"
+import axios, { AxiosInstance } from "axios";
 
 const weather: AxiosInstance = axios.create({
   baseURL: "http://api.openweathermap.org/",
-})
-let appId: string =
-  "&units=metric&lang=en&appid=4aaa6ddbde83aae9ede27f89d73c503e"
+});
+let appId = process.env.REACT_APP_WEATHER_API;
 
 type cityName = {
-  name: string
+  name: string;
   sys: {
-    country: string
-  }
-}
+    country: string;
+  };
+};
 
 type initWeatherType = {
-  lat: number
-  lon: number
-}
+  lat: number;
+  lon: number;
+};
 
 export const weatherAPI = {
   getInitWeather(lat: number, lon: number) {
     return weather.get<initWeatherType>(
       `data/2.5/onecall?lat=${lat}&lon=${lon}${appId}`
-    )
+    );
   },
   getCityName(lat: number, lon: number) {
     return weather.get<cityName>(
       `data/2.5/weather?lat=${lat}&lon=${lon}${appId}`
-    )
+    );
   },
-}
+};
