f3ae0192b7a3aef2c4390a4aff3569b5ad48940f
GunSlinger <you@example.com>
2021-08-17 14:02:27 +0300

    server.js settings and fix route for cities API

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || "3001";
+const PORT = process.env.PORT || "8080";
 
 server.use(middlewares);
 server.use(router);
--- src/api/citiesAPI.ts
+++ src/api/citiesAPI.ts
@@ -1,12 +1,12 @@
 import { citiesType } from "./../types/types";
 import axios, { AxiosInstance } from "axios";
 
-const port = process.env.PORT || process.env.REACT_APP_PORT;
+const baseURL = process.env.PORT
+  ? "https://react-ts-weather-app.herokuapp.com/"
+  : `http://localhost:${process.env.REACT_APP_PORT}/`;
 
-const cities: AxiosInstance = axios.create({
-  baseURL: `http://localhost:${port}/`,
-});
-console.log(process.env.PORT);
+const cities: AxiosInstance = axios.create({ baseURL });
+
 export const citiesAPI = {
   getCities() {
     return cities.get<citiesType[]>("cities");
