dc8a15d7491f3e5525c9e57b7a6a7a83c52be8f5
GunSlinger <you@example.com>
2021-08-17 15:34:07 +0300

    create server.js file and add servers ports

--- src/api/citiesAPI.ts
+++ src/api/citiesAPI.ts
@@ -1,22 +1,24 @@
-import { citiesType } from "./../types/types"
-import axios, { AxiosInstance } from "axios"
+import { citiesType } from "./../types/types";
+import axios, { AxiosInstance } from "axios";
+
+const port = process.env.PORT || process.env.REACT_APP_PORT;
 
 const cities: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/",
-})
-
+  baseURL: `http://localhost:${port}/`,
+});
+console.log(process.env.PORT);
 export const citiesAPI = {
   getCities() {
-    return cities.get<citiesType[]>("cities")
+    return cities.get<citiesType[]>("cities");
   },
   deleteCity(id: number) {
-    return cities.delete<citiesType>(`cities/${id}`)
+    return cities.delete<citiesType>(`cities/${id}`);
   },
   setCity(city: string, lat: number, lon: number) {
     return cities.post<citiesType>("cities", {
       city: city,
       lat: lat,
       lon: lon,
-    })
+    });
   },
-}
+};
